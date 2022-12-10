c6cd4a219c8126b43c2925c9d54f2ccc586019c6
Leonardo Ruoso Vendramini <leonardo_vendramini@hotmail.com>
2022-12-10 08:52:01 -0300

    Criação dos repositórios concretos, contexto e DbSets e adicionado dependência pomelo mysql e proxies.

---
+++ QuickBuy.Repositorio/Contexto/QuickBuyContexto.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QuickBuy.Dominio.Entidades;
+using QuickBuy.Dominio.ObjetoDeValor;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Repositorio.Contexto
+{
+    internal class QuickBuyContexto : DbContext
+    {
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<ItemPedido> ItensPedidos { get; set;}
+        public DbSet<FormaPagamento> FormaPagamento { get; set; }
+
+    }
+}
---
+++ QuickBuy.Repositorio/Repositorios/BaseRepositorio.cs
@@ -0,0 +1,40 @@
+﻿using QuickBuy.Dominio.Contratos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Repositorio.Repositorios
+{
+    public class BaseRepositorio<T> : IBaseRepositorio<T> where T : class
+    {
+        public void Adicionar(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Atualizar(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T ObterPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> ObterTodos()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remover(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
