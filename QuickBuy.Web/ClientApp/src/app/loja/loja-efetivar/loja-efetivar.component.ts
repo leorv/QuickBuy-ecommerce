0c70da327d42bef7499f32bc2bf94c4760bc74e6
Leonardo Ruoso Vendramini <leonardo_vendramini@hotmail.com>
2022-12-30 22:48:39 -0300

    carrinho de comprar, tabela de produtos no carrinho e resumo do pedido.

--- QuickBuy.Web/ClientApp/src/app/loja/loja-efetivar/loja-efetivar.component.ts
+++ QuickBuy.Web/ClientApp/src/app/loja/loja-efetivar/loja-efetivar.component.ts
@@ -1,15 +1,39 @@
+import { Produto } from './../../models/Produto';
+import { CarrinhoCompras } from './../carrinho-compras';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-loja-efetivar',
-  templateUrl: './loja-efetivar.component.html',
-  styleUrls: ['./loja-efetivar.component.css']
+    selector: 'app-loja-efetivar',
+    templateUrl: './loja-efetivar.component.html',
+    styleUrls: ['./loja-efetivar.component.css']
 })
 export class LojaEfetivarComponent implements OnInit {
 
-  constructor() { }
+    carrinhoCompras: CarrinhoCompras;
+    produtos: Produto[];
 
-  ngOnInit() {
-  }
+    constructor() { }
+
+    ngOnInit() {
+        this.carrinhoCompras = new CarrinhoCompras();
+        this.produtos = this.carrinhoCompras.obterProdutos();
+    }
+
+    atualizarPreco(produto: Produto, quantidade: number) {
+        if (!produto.precoOriginal) {
+            produto.precoOriginal = produto.preco;
+        }
+        if (quantidade <= 0) {
+            quantidade = 1;
+            produto.quantidade = quantidade;
+        }
+        produto.preco = produto.precoOriginal * quantidade;
+        this.carrinhoCompras.atualizar(this.produtos);
+    }
+
+    remover(produto: Produto) {
+        this.carrinhoCompras.removerProduto(produto);
+        this.produtos = this.carrinhoCompras.obterProdutos();
+    }
 
 }
--- QuickBuy.Web/ClientApp/src/app/loja/loja-produto/loja-produto.component.ts
+++ QuickBuy.Web/ClientApp/src/app/loja/loja-produto/loja-produto.component.ts
@@ -1,6 +1,8 @@
+import { Router } from '@angular/router';
 import { Produto } from './../../models/Produto';
 import { ProdutoService } from './../../servicos/produto/produto.service';
 import { Component, OnInit } from '@angular/core';
+import { CarrinhoCompras } from '../carrinho-compras';
 
 @Component({
     selector: 'app-loja-produto',
@@ -10,12 +12,15 @@ import { Component, OnInit } from '@angular/core';
 export class LojaProdutoComponent implements OnInit {
 
     produto: Produto = new Produto();
+    carrinhoCompras: CarrinhoCompras
 
     constructor(
-        private produtoService: ProdutoService
+        private produtoService: ProdutoService,
+        private router: Router
     ) { }
 
     ngOnInit() {
+        this.carrinhoCompras = new CarrinhoCompras();
         var produtoDetalhe = sessionStorage.getItem('produtoDetalhe');
         if (produtoDetalhe) {
             this.produto = JSON.parse(produtoDetalhe);
@@ -23,6 +28,8 @@ export class LojaProdutoComponent implements OnInit {
     }
 
     comprar() {
+        this.carrinhoCompras.adicionar(this.produto);
+        this.router.navigate(['/loja-efetivar'])
         
     }
 
